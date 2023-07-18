bf4f93fced9f30310866ca1e4c85a78209711e7b
Felipe <felipecortes0604@gmail.com>
2023-07-18 10:53:01 -0500

    ejercicio con cupones uno

--- porcentaje.js
+++ porcentaje.js
@@ -7,19 +7,36 @@ const btn = document.querySelector(".calcular")
 
 btn.addEventListener('click', calcularPrecioConDescuento)
 
-function calcularPrecioConDescuento(){
-    let precio=Number(inputPrice.value);
-    let descuento= Number(inputCoupon.value);
-    console.log({precio,descuento})
+function calcularPrecioConDescuento() {
+    let precio = Number(inputPrice.value);
+    let coupon = inputCoupon.value;
+    
+    let descuento;
 
-    if(precio!='' && descuento!=''){
-        if(descuento<=100){
-            let newDescuento= (precio*(100-descuento)/100);
-            pResult.innerText=`El nuevo precio es ${newDescuento}`
-        }else{
-            pResult.innerText='!El descuento no puede superar el 100%!'
+    switch (coupon) {
+        case 'Go':
+            descuento = 10;
+            break;
+        case 'Plus':
+            descuento= 30;
+            break;
+        case 'PlusDuo':
+            descuento=50;
+            break;
+        default:
+            pResult.innerText = '!El cupon no es valido!'
+            return;
+    }
+    console.log({ precio, descuento })
+
+    if (precio != '' && descuento != '') {
+        if (descuento <= 100) {
+            let newDescuento = (precio * (100 - descuento) / 100);
+            pResult.innerText = `El nuevo precio es ${newDescuento}`
+        } else {
+            pResult.innerText = '!El descuento no puede superar el 100%!'
         }
-    }else{
-        pResult.innerText='¡LLENA LOS DATOS SOLICITADOS!'
+    } else {
+        pResult.innerText = '¡LLENA LOS DATOS SOLICITADOS!'
     }
 }
